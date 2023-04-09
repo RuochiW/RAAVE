b3aff6b81ed4337ba6616150e4121a606362ed2c
RW <ruochi.w@gmail.com>
2023-04-08 19:19:08 -0700

    update

--- data/data_controller/notification_data_controller.py
+++ data/data_controller/notification_data_controller.py
@@ -121,7 +121,7 @@ def read_notification(notification_obj):
     Reads a notification object from the database.
 
     Args:
-        notification_obj: An Notification object to write to the database.
+        notification_obj: An instance of the Notification class containing the notify ID.
 
     Returns:
         Success case: A list containing the boolean value True followed by the notification data.
@@ -135,72 +135,182 @@ def read_notification(notification_obj):
 
     """
 
+    # Attempt to read the notification object from the database
     try:
+
+        # Check if notification_obj is an instance of the Notification class
         if isinstance(notification_obj, Notification):
+
+            # Connect to the database and create a cursor object
             conn = sqlite3.connect(db_path)
             c = conn.cursor()
+
+            # Execute a SQL query to retrieve the notification data from the database
             c.execute('''SELECT notify_id, event, account, strftime('%Y-%m-%d %H:%M', notify_date) AS notify_date,info 
                          FROM raave_notification
                          WHERE notify_id = ?''', (notification_obj.notify_id,))
+
+            # Get the result of the query
             result = c.fetchall()
+
+            # Check if the query returns any results
             if result:
+
+                # Convert the tuples in the result to lists
                 notification_data = [list(t) for t in result]
+
+                # Close the database connection
                 conn.close()
+
+                # Create a list containing True followed by the list of notification data
                 bool_true = [True]
                 bool_true.extend(notification_data)
+
                 return bool_true
+
+            # If the query returns no results
             else:
+
+                # Close the database connection
                 conn.close()
+
+                # Log the error
                 e = 'Notification not found.'
                 logger.error("An error occurred: %s", e)
+
                 return [False, e]
+
+        # If notification_obj is not an instance of the nNotification class
         else:
+
+            # Log the error
             e = 'Invalid object type.'
             logger.error("An error occurred: %s", e)
+
             return [False, e]
+
+    # If any other exceptions occur
     except Exception as e:
+
+        # Log the error
         logger.error("An error occurred: %s", str(e))
+
         return [False, str(e)]
 
 
 def read_all_notification(obj):
+    """
+    Reads all notifications belonging to the account or event from the database.
+
+    Args:
+        obj: An instance of the Account class containing the account ID or Event class containing the event ID.
+
+    Returns:
+        Success case: A list containing the boolean value True followed by the notifications' notify ID.
+        [True, [notify_id1, notify_id2, ...]]
+
+        Fail case: A list containing the boolean value False followed by the error message.
+        [False, [error message]]
+
+    Raises:
+        Exception: If an error occurs during the database transaction or any other error.
+
+    """
+
+    # Attempt to read the category object from the database
     try:
+
+        # Check if obj is an instance of the Event class
         if isinstance(obj, Event):
+
+            # Connect to the database and create a cursor object
             conn = sqlite3.connect(db_path)
             c = conn.cursor()
+
+            # Execute a SQL query to retrieve the notifications info to database
             c.execute('''SELECT notify_id FROM raave_notification WHERE event = ?''', (obj.event_id,))
+
+            # Get the result of the query
             result = c.fetchall()
+
+            # Check if the query returns any results
             if result:
+
+                # Convert the tuples in the result to lists
                 notify_data = [list(t) for t in result]
+
+                # Close the database connection
                 conn.close()
+
+                # Create a list containing True followed by the list of notifications info
                 bool_true = [True]
                 bool_true.extend(notify_data)
+
                 return bool_true
+
+            # If the query returns no results
             else:
+
+                # Close the database connection
                 conn.close()
+
+                # Log the error
                 e = 'No notifications found for the specified event.'
                 logger.error("An error occurred: %s", e)
+
                 return [False, e]
+
+        # Check if account_obj is an instance of the Account class
         elif isinstance(obj, Account):
+
+            # Connect to the database and create a cursor object
             conn = sqlite3.connect(db_path)
             c = conn.cursor()
+
+            # Execute a SQL query to retrieve the notifications info to database
             c.execute('''SELECT notify_id FROM raave_notification WHERE account = ?''', (obj.account_id,))
             result = c.fetchall()
+
+            # Check if the query returns any results
             if result:
+
+                # Convert the tuples in the result to lists
                 notify_data = [list(t) for t in result]
+
+                # Close the database connection
                 conn.close()
+
+                # Create a list containing True followed by the list of notifications info
                 bool_true = [True]
                 bool_true.extend(notify_data)
+
                 return bool_true
+
+            # If the query returns no results
             else:
+
+                # Close the database connection
                 conn.close()
+
+                # Log the error
                 e = 'No notifications found for the specified account.'
                 logger.error("An error occurred: %s", e)
+
                 return [False, e]
+
+        # If account_obj is not an instance of the Account class
         else:
+
+            # Log the error
             e = 'Invalid object type.'
             logger.error("An error occurred: %s", e)
+
             return [False, e]
+
+    # If any other exceptions occur
     except Exception as e:
+
+        # Log the error
         logger.error("An error occurred: %s", str(e))
+
         return [False, str(e)]
